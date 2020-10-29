7a640c3d13d415e4ea6822fef358e30869a01b5d
Cibil P <cpankiras.contractor@libertyglobal.com>
2020-10-29 17:54:50 +0100

    Telemetry 2.0 - HTTPS mutual authentication support - Part-1 ( RDKB-4875 )
    DESCRIPTION:
    
    As per LG requirement, Telemetry agent has to mutual authenticate with LG servers before sending/receiving data.
    
    CHANGES:
    
    1. Added Server root ca cert to curl object to handle server authentication
    2. Added Device/Manufacturer cert to curl object to handle client authentication
    3. Fixed the logic to identify erouter0 interface's address type
    
    TESTING PERFORMED:
    
    1. Give a valid DCMResponse.txt file url
    2. DUT should download the file without any issues
    3. DUT should push the json data to the odh server without any issues
    
    NOTE:
    
    This commit has generic changes to handle the private key.
    
    Signed-off-by: Cibil cpankiras.contractor@libertyglobal.com

--- source/protocol/http/curlinterface.c
+++ source/protocol/http/curlinterface.c
@@ -131,6 +131,12 @@ static T2ERROR setHeader(CURL *curl, const char* destURL, struct curl_slist **he
        return T2ERROR_FAILURE;
     }
 
+    /* Load server ca, device certificate and private key to curl object */
+    if(addCertificatesToHTTPHeader(curl) != T2ERROR_SUCCESS )
+    {
+        return T2ERROR_FAILURE;
+    }
+
     T2Debug("%s --out\n", __FUNCTION__);
     return T2ERROR_SUCCESS;
 }
@@ -190,6 +196,108 @@ static T2ERROR setPayload(CURL *curl, const char* payload)
     return T2ERROR_SUCCESS;
 }
 
+static CURLcode load_private_key(void *sslctx)
+{
+
+    CURLcode return_status = CURLE_SSL_CERTPROBLEM;
+
+    /* If the private key cannot be loaded directly, then we need to decrypt it. */
+    if( (SSL_CTX_use_PrivateKey_file((SSL_CTX *)sslctx, PRIVATE_KEY, SSL_FILETYPE_PEM) != 1) )
+    {
+        T2Error("Unable to add Private key to the request \n");
+    }
+    else
+    {
+        T2Info("Private key successfully added to the request\n");
+        return_status = CURLE_OK;
+    }
+
+    return return_status;
+}
+
+static CURLcode load_certificates(CURL *curl, void *sslctx, void *parm)
+{
+
+    X509 *dev_cert = NULL;
+    X509 *man_cert = NULL;
+    FILE *f_cert = fopen(DEVICE_CERT, "rb");
+    FILE *f_man = fopen(MANUFACTURER_CERT, "rb");
+    int ret = -1;
+    CURLcode return_status = CURLE_OK;
+
+    if (f_cert == NULL || f_man == NULL)
+    {
+        if (f_cert)
+        {
+            T2Error("Unable to open device certificate!!!\n");
+            fclose(f_cert);
+        }
+        if (f_man)
+        {
+            T2Error("Unable to open manufacturer certificate!!!\n");
+            fclose(f_man);
+        }
+
+        return CURLE_SSL_CERTPROBLEM;
+    }
+    /* Load device certificate to ssl-ctx structure*/
+    if (f_cert)
+    {
+        dev_cert = d2i_X509_fp(f_cert, NULL);
+        if (dev_cert)
+        {
+            ret = SSL_CTX_use_certificate((SSL_CTX *)sslctx, dev_cert);
+        }
+
+        if (ret != 1)
+        {
+            return_status = CURLE_SSL_CERTPROBLEM;
+        }
+        fclose(f_cert);
+    }
+
+    if (return_status == CURLE_OK)
+    {
+        /* Form a certificate chain by Concatenating manufacturer certificate to device certificate */
+        if (f_man)
+        {
+            ret = -1;
+            man_cert = d2i_X509_fp(f_man, NULL);
+            if (man_cert)
+            {
+                /* The x509 certificate provided to SSL_CTX_add_extra_chain_cert() will be freed by the library when the SSL_CTX is destroyed.
+                we should not free man_cert */
+                ret = SSL_CTX_add_extra_chain_cert((SSL_CTX *)sslctx, man_cert);
+            }
+
+            if (ret != 1)
+            {
+                T2Error("Failed to concatenate manufacturer certificate to device certificate!!!\n");
+                return_status = CURLE_SSL_CERTPROBLEM;
+            }
+        }
+    }
+    if (dev_cert)
+    {
+        /* SSL_CTX_use_certificate function copy the certificate to ssl using SSL_new() function. So it is safe to free dev_cert */
+        X509_free(dev_cert);
+        dev_cert = NULL;
+    }
+    if (return_status == CURLE_OK)
+    {
+        T2Info("Device certificates successfully added to the request\n");
+        return_status = load_private_key(sslctx);
+    }
+    else
+    {
+        T2Error("Unable to add device certificates to the request, error code - %d\n", ret);
+    }
+    /* Close the manufacturer certificate */
+    if(f_man)
+        fclose(f_man);
+
+    return return_status;
+}
 
 T2ERROR sendReportOverHTTP(char *httpUrl, char *payload, pid_t* outForkedPid) {
     CURL *curl = NULL;
@@ -356,4 +464,52 @@ T2ERROR sendCachedReportsOverHTTP(char *httpUrl, Vector *reportList)
         free(payload);
     }
     return T2ERROR_SUCCESS;
+}
+
+T2ERROR addCertificatesToHTTPHeader(CURL *curl)
+{
+
+    CURLcode code = CURLE_OK;
+    T2ERROR ret = T2ERROR_SUCCESS;
+
+    code = curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
+    if(code != CURLE_OK)
+    {
+        T2Error("%s : Curl set opts failed with error %s \n", __FUNCTION__, curl_easy_strerror(code));
+        ret = T2ERROR_FAILURE;
+    }
+
+    if(ret == T2ERROR_SUCCESS)
+    {
+        code = curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 2L);
+        if(code != CURLE_OK)
+        {
+            T2Error("%s : Curl set opts failed with error %s \n", __FUNCTION__, curl_easy_strerror(code));
+            ret = T2ERROR_FAILURE;
+        }
+    }
+
+    if(ret == T2ERROR_SUCCESS)
+    {
+        code = curl_easy_setopt(curl, CURLOPT_CAINFO, SERVER_CA_CERT);
+        if(code != CURLE_OK)
+        {
+            T2Error("%s : Curl set opts failed with error %s \n", __FUNCTION__, curl_easy_strerror(code));
+            ret = T2ERROR_FAILURE;
+        }
+    }
+
+    if(ret == T2ERROR_SUCCESS)
+    {
+        /* Add device certificates and key to the handle */
+        code = curl_easy_setopt(curl, CURLOPT_SSL_CTX_FUNCTION, *load_certificates);
+        if(code != CURLE_OK)
+        {
+            T2Error("%s : Curl set opts failed with error %s \n", __FUNCTION__, curl_easy_strerror(code));
+            ret = T2ERROR_FAILURE;
+        }
+    }
+
+    return ret;
+
 }

ef4e26a08cd22e7a70ca91600fe56887d6fab6d9
Cibil P <cpankiras.contractor@libertyglobal.com>
2020-11-10 12:33:52 +0100

    Telemetry 2.0 - Download DCM Configuration file on maintenance window ( RDKB-4937 )
    DESCRIPTION:
    
    As per LG requirement, DCMResponse.txt file has to be downloaded every day.
    
    CHANGES:
    
    1. Parsed StartTime and MaxRandomDelay values from DCMResponse.txt file.
    2. Added the logic to create a cron string based on StartTime and MaxRandomDelay values.
    3. Added the logic to download DCMResponse.txt file based on the cron string.
    
    TESTING PERFORMED:
    
    1. Create a DCMResponse.txt file with valid StartTime(01:00) and MaxRandomDelay(180).
    2. Configure this DCMResponse.txt file url to telemetry agent
    3. DUT should download the file without any issues
    4. Check the cronjob running in the DUT (crontab -l)
    5. The following job should be listed in the cronjob.
    /lib/rdk/autodownload_dcmconfig.sh
    Signed-off-by: Cibil cpankiras.contractor@libertyglobal.com

---
+++ source/bulkdata/scripts/autodownload_dcmconfig.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+##########################################################################
+#
+# Copyright 2020 Liberty Global B.V.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+
+T2_0_BIN="telemetry2_0"
+
+#TO DO: Once we ported enable dml, uncomment the following
+
+# Download DCMResponse.txt file only if telemetry is enabled.
+# telemetryStatus=`syscfg get telemetry_enable`
+# if [ "$telemetryStatus" != "true" ]; then
+#   exit 0
+# fi
+
+T2_0_BIN_PID=`pidof $T2_0_BIN`
+
+if [ -n "${T2_0_BIN_PID}" ]; then
+    # Singal 15 (SIGTERM) is handled in telemetry2_0 process to download DCMResponse.txt file
+    kill -15 $T2_0_BIN_PID
+else
+    echo "Not able to get $T2_0_BIN pid!!!"
+fi

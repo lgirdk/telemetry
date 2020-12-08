1ee7c17f029653f2193a96e23aeeffab5e09ff00
Anu Shaju Ainikkal <ashaju.contractor@libertyglobal.com>
2020-12-08 15:09:30 +0100

    Telemetry 2.0 - Functionality implementation of retry logic ( RDKB-4935)
    BUG DESCRIPTION:
    
      - Data Push ODH - retry logic
    
    FIX DESCRIPTION:
    
      -Added  funcionality implementation of retry logic
    
    TESTING PERFORMED:
    
      1. SPV following parameters
         -dmcli eRT setv  Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.Enable bool true
         -dmcli eRt setv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.ConfigURL
          string https://172.30.117.124/ssl/DCMresponse-test.json
         -dmcli eRT setv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.Version string "2"
    
      2. GPV Device.X_LGI-COM_Telemetry.DcmDownloadStatus.AttemptCount
         eg : dmcli eRT getv Device.X_LGI-COM_Telemetry.DcmDownloadStatus.AttemptCount
    
    Signed-off-by: Anu Shaju Ainikkal(ashaju.contractor@libertyglobal.com)
    
    Implementaion of DcmDownloadStatus.AttemptCount and UploadStatus.AttemptCount parameters ( OFW-894 )
    
    BUG DESCRIPTION:
      -UploadStatus.AttemptCount/DcmDownloadStatus.AttemptCount
       is not incrementing while DCM download/ODH upload
    
    FIX DESCRIPTION:
    
      -Added implementation for DcmDownloadStatus.AttemptCount and
       UploadStatus.AttemptCount parameters
    
    TESTING PERFORMED:
    
      -dmcli eRT setv Device.X_LGI-COM_Telemetry.Enable bool true
      -dmcli eRT setv Device.X_LGI-COM_Telemetry.DCMConfigFileURL string http://172.30.117.124/ssl/DCMresponse-test_new.json
      -dmcli eRT setv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.Version string 2
      -dmcli eRT getv Device.X_LGI-COM_Telemetry.DcmDownloadStatus.AttemptCount
      -dmcli eRT getv Device.X_LGI-COM_Telemetry.UploadStatus.AttemptCount
    
    Signed-off-by: Anu Shaju Ainikkal(ashaju.contractor@libertyglobal.com)

--- source/xconf-client/xconfclient.c
+++ source/xconf-client/xconfclient.c
@@ -971,6 +971,13 @@ static void* getUpdatedConfigurationThread(void *data)
     while(!stopFetchRemoteConfiguration && isValidUrl)
     {
         T2ERROR ret = fetchRemoteConfiguration(configURL, &configData);
+
+        xConfRetryCount++;
+        snprintf(buf,sizeof(buf),"%d",xConfRetryCount);
+        if (telemetry_syscfg_set("dcm_attemptCount", buf) != 0)
+        {
+            T2Error("dcm_attemptCount set failed\n");
+        }
         if(ret == T2ERROR_SUCCESS)
         {
             ProfileXConf *profile = 0;
@@ -1040,7 +1047,6 @@ static void* getUpdatedConfigurationThread(void *data)
                 free(configData);
                 configData = NULL ;
             }
-            xConfRetryCount++;
             if(xConfRetryCount >= maxAttempts)
             {
                 T2Error("Reached max xconf retry counts : %d, Using saved profile if exists until next reboot\n", maxAttempts);

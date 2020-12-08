9739ce8872cb57086f4ca4d73597be3e3a8aa209
Anu Shaju Ainikkal <ashaju.contractor@libertyglobal.com>
2020-12-08 15:09:30 +0100

    Telemetry 2.0 - Functionality implementation of retry logic ( RDKB-4935)
    BUG DESCRIPTION:
    
      - Data Push ODH - retry logic
    
    FIX DESCRIPTION:
    
      -Added  funcionality implementation of retry logic
    
    TESTING PERFORMED:
    
      1. SPV following parameters
         -dmcli eRT setv  Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.Enable bool true
         -dmcli eRt setv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.ConfigURL
          string https://172.30.117.124/ssl/DCMresponse-test.json
         -dmcli eRT setv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.Version string "2"
    
      2. GPV Device.X_LGI-COM_Telemetry.DcmDownloadStatus.AttemptCount
         eg : dmcli eRT getv Device.X_LGI-COM_Telemetry.DcmDownloadStatus.AttemptCount
    
    Signed-off-by: Anu Shaju Ainikkal(ashaju.contractor@libertyglobal.com)
    
    Implementaion of DcmDownloadStatus.AttemptCount and UploadStatus.AttemptCount parameters ( OFW-894 )
    
    BUG DESCRIPTION:
      -UploadStatus.AttemptCount/DcmDownloadStatus.AttemptCount
       is not incrementing while DCM download/ODH upload
    
    FIX DESCRIPTION:
    
      -Added implementation for DcmDownloadStatus.AttemptCount and
       UploadStatus.AttemptCount parameters
    
    TESTING PERFORMED:
    
      -dmcli eRT setv Device.X_LGI-COM_Telemetry.Enable bool true
      -dmcli eRT setv Device.X_LGI-COM_Telemetry.DCMConfigFileURL string http://172.30.117.124/ssl/DCMresponse-test_new.json
      -dmcli eRT setv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.Version string 2
      -dmcli eRT getv Device.X_LGI-COM_Telemetry.DcmDownloadStatus.AttemptCount
      -dmcli eRT getv Device.X_LGI-COM_Telemetry.UploadStatus.AttemptCount
    
    Signed-off-by: Anu Shaju Ainikkal(ashaju.contractor@libertyglobal.com)

--- source/bulkdata/profilexconf.c
+++ source/bulkdata/profilexconf.c
@@ -259,6 +259,8 @@ static void* CollectAndReportXconf(void* data)
         }
         if(strcmp(profile->protocol, "HTTP") == 0)
         {
+           char buf[12];
+
            ret = sendReportOverHTTP(profile->t2HTTPDest->URL, jsonReport);
 
            if(ret == T2ERROR_FAILURE)
@@ -273,9 +275,22 @@ static void* CollectAndReportXconf(void* data)
               Vector_PushBack(profile->cachedReportList, strdup(jsonReport));
 
               T2Info("Report Cached, No. of reportes cached = %lu\n", (unsigned long)Vector_Size(profile->cachedReportList));
+
+              snprintf(buf,sizeof(buf),"%d",Vector_Size(profile->cachedReportList));
+              if(syscfg_set(NULL, "upload_attemptCount", buf) != 0)
+              {
+                  T2Error("Failed to set upload attempt count\n");
+              }
+           }
+           else if(ret == T2ERROR_SUCCESS)
+           {
+               snprintf(buf,sizeof(buf),"%d",Vector_Size(profile->cachedReportList)+1);
+               syscfg_set(NULL, "upload_attemptCount", buf);
            }
            else if(Vector_Size(profile->cachedReportList) > 0)
            {
+               snprintf(buf,sizeof(buf),"%d",Vector_Size(profile->cachedReportList)+1);
+               syscfg_set(NULL, "upload_attemptCount", buf);
                T2Info("Trying to send  %lu cached reports\n", (unsigned long)Vector_Size(profile->cachedReportList));
                ret = sendCachedReportsOverHTTP(profile->t2HTTPDest->URL, profile->cachedReportList);
            }

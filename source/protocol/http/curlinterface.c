a6bd17e57e45cfc9ed6e82a2bc897482820a93b5
Anu Shaju Ainikkal <ashaju.contractor@libertyglobal.com>
2021-02-15 08:48:39 +0100

    fix empty values for UploadStatus and DcmDownloadStatus parameters ( OFW-468 )
    BUG DESCRIPTION:
      -HTTPStatusString, LastAttemptTimestamp , HTTPStatus, AttemptCount  is empty even though upload is successful,
       same is seen for DCM download as well.
    
    FIX DESCRIPTION:
      -Added implementation for UploadStatus parameters.
    
    TESTING PERFORMED:
    
      -dmcli eRT setv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.Enable bool true
      -dmcli eRT setv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.ConfigURL string "https://xxxx/ssl/DCMresponse-test.json"
      -dmcli eRT setv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.Version string  2
      -dmcli eRT setv Device.X_LGI-COM_Telemetry.Enable bool true
      -dmcli eRT setv Device.X_LGI-COM_Telemetry.DCMConfigFileURL string https://yyyy/DCMresponse.txt
      -dmcli eRT getv Device.X_LGI-COM_Telemetry.
    
    Signed-off-by: Anu Shaju Ainikkal(ashaju.contractor@libertyglobal.com)

--- source/protocol/http/curlinterface.c
+++ source/protocol/http/curlinterface.c
@@ -371,15 +371,18 @@ T2ERROR sendReportOverHTTP(char *httpUrl, char* payload) {
                 res = curl_easy_perform(curl);
                 curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
 
-                snprintf(buf,sizeof(buf),"%d",res);
+                snprintf(buf,sizeof(buf),"%d",http_code);
                 if (syscfg_set(NULL, "upload_httpStatus", buf) == 0)
                 {
                     T2Info("upload_httpStatus set successfully\n");
                 }
-                snprintf(buf,sizeof(buf),"%s",errbuf);
-                if (syscfg_set(NULL, "upload_httpStatusString", buf) == 0)
+                if (http_code == 200)
                 {
-                   T2Info("upload_httpStatusString set successfully\n");
+                    syscfg_set(NULL, "upload_httpStatusString", "OK");
+                }
+                else
+                {
+                    syscfg_set(NULL, "upload_httpStatusString", errbuf);
                 }
                 Timestamp_Status = getcurrenttime(current_time, sizeof(current_time));
                 if (Timestamp_Status != 0)

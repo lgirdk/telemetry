85e96a2b40ab1614c726d2e427f5fa6ddb3b199b
Andy Carvalho <acarvalho.contractor@libertyglobal.com>
2021-03-30 15:45:05 +0200

    remove build dependency on utopia ( OFW-861 )
    BUG DESCRIPTION: As part of LGI changes, we have added a dependency on utopia in telemetry to fetch syscfg variables.
    Recently Comcast added some markers in utopia component which adds a build/run time dependency on telemetry.
    
    This causes a circular dependency during the build.
    
    FIX DESCRIPTION:
        - Removed utopia dependency in telemetry by modifying direct syscfg get/set calls.
    
    TESTING PERFORMED:
        1. Bring up the modem.
        2. Set "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.ConfigURL" path.
        3. Set "Device.X_LGI-COM_Telemetry.Enable" to true, through dmcli.
        4. GPV on "Device.X_LGI-COM_Telemetry." to check parameter values.
    
    Signed-off-by: Andy Carvalho <acarvalho.contractor@libertyglobal.com>

--- source/bulkdata/profilexconf.c
+++ source/bulkdata/profilexconf.c
@@ -33,6 +33,7 @@
 #include "vector.h"
 #include "dcautil.h"
 #include "t2parserxconf.h"
+#include "t2commonutils.h"
 
 #define T2REPORT_HEADER "T2"
 #define T2REPORT_HEADERVAL  "1.0"
@@ -242,20 +243,17 @@ static void* CollectAndReportXconf(void* data)
               T2Info("Report Cached, No. of reportes cached = %d\n", Vector_Size(profile->cachedReportList));
 
               snprintf(buf,sizeof(buf),"%d",Vector_Size(profile->cachedReportList));
-              if(syscfg_set(NULL, "upload_attemptCount", buf) != 0)
-              {
-                  T2Error("Failed to set upload attempt count\n");
-              }
+              telemetry_syscfg_set("upload_attemptCount", buf);
            }
            else if(ret == T2ERROR_SUCCESS)
            {
                snprintf(buf,sizeof(buf),"%d",Vector_Size(profile->cachedReportList)+1);
-               syscfg_set(NULL, "upload_attemptCount", buf);
+               telemetry_syscfg_set("upload_attemptCount", buf);
            }
            else if(Vector_Size(profile->cachedReportList) > 0)
            {
                snprintf(buf,sizeof(buf),"%d",Vector_Size(profile->cachedReportList)+1);
-               syscfg_set(NULL, "upload_attemptCount", buf);
+               telemetry_syscfg_set("upload_attemptCount", buf);
                T2Info("Trying to send  %d cached reports\n", Vector_Size(profile->cachedReportList));
                ret = sendCachedReportsOverHTTP(profile->t2HTTPDest->URL, profile->cachedReportList);
            }

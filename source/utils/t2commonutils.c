45b1fb70b83c5e1b1d2bc21212d68acabd217c8f
Andy Carvalho <acarvalho.contractor@libertyglobal.com>
2021-03-30 15:45:05 +0200

    remove build dependency on utopia ( OFW-861 )
    BUG DESCRIPTION: As part of LGI changes, we have added a dependency on utopia in telemetry to fetch syscfg variables.
    Recently Comcast added some markers in utopia component which adds a build/run time dependency on telemetry.
    
    This causes a circular dependency during the build.
    
    FIX DESCRIPTION:
        - Removed utopia dependency in telemetry by modifying direct syscfg get/set calls.
    
    TESTING PERFORMED:
        1. Bring up the modem.
        2. Set "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.ConfigURL" path.
        3. Set "Device.X_LGI-COM_Telemetry.Enable" to true, through dmcli.
        4. GPV on "Device.X_LGI-COM_Telemetry." to check parameter values.
    
    Signed-off-by: Andy Carvalho <acarvalho.contractor@libertyglobal.com>

--- source/utils/t2commonutils.c
+++ source/utils/t2commonutils.c
@@ -18,6 +18,7 @@
 */
 
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "t2log_wrapper.h"
@@ -49,4 +50,42 @@ int getcurrenttime (char *current_time_string, int timestampparams)
     strftime(current_time_string, timestampparams, "%Y-%m-%d %H:%M:%S", c_time_string);
 
     return 0;
+}
+
+int telemetry_syscfg_get (char *temp, char *buf, int buf_size)
+{
+    FILE *value = NULL;
+    char cmd[256];
+    int ret = -1;
+
+    snprintf(cmd, sizeof(cmd), "syscfg get %s", temp);
+
+    value = popen(cmd, "r");
+    if (value)
+    {
+        fgets(buf, buf_size, value);
+        buf[strlen(buf) - 1] = '\0';
+        pclose(value);
+        ret = 0;
+    }
+
+    return ret;
+}
+
+int telemetry_syscfg_set (char *temp, char *buf)
+{
+    FILE *value = NULL;
+    char cmd[256];
+    int ret = -1;
+
+    snprintf(cmd, sizeof(cmd), "syscfg set %s \"%s\"", temp, buf);
+
+    value = popen(cmd, "w");
+    if (value)
+    {
+        pclose(value);
+        ret = 0;
+    }
+
+    return ret;
 }

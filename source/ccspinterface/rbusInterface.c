81bad159c412539d1ab70d4d23b6ef429114c145
Andre McCurdy <armccurdy@gmail.com>
2021-04-27 12:08:43 -0700

    drop dependency on glib
    RDKV depends on glib for base64 APIs and some of the local test apps
    includes glib headers. It's only safe to drop the glib dependency if
    building for RDKB and test apps are disabled.
    
    Do not upstream.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/ccspinterface/rbusInterface.c
+++ source/ccspinterface/rbusInterface.c
@@ -20,10 +20,10 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
-#include <glib.h>
-#include <glib/gi18n.h>
 #include <pthread.h>
 #include <unistd.h>
+
+#include <ansc_platform.h>
 
 #include "t2collection.h"
 #include "t2common.h"
@@ -400,27 +400,27 @@ rbusError_t t2PropertyDataSetHandler(rbusHandle_t handle, rbusProperty_t prop, r
 
         if(type_t == RBUS_STRING) {
             char* data = rbusValue_ToString(paramValue_t, NULL, 0);
-            guchar *webConfigString = NULL;
-            gsize decodedDataLen = 0;
+            char *webConfigString = NULL;
+            int stringSize = 0;
             if(data) {
                 T2Debug("Call datamodel function  with data %s \n", data);
-                webConfigString = g_base64_decode(data, &decodedDataLen);
-                if(NULL == webConfigString ||  0 == decodedDataLen ){
+                webConfigString = AnscBase64Decode(data, &stringSize);
+                if(NULL == webConfigString ||  0 == stringSize ){
                     T2Error("Invalid base64 input string. Ignore processing input configuration.\n");
                     if(webConfigString != NULL){
-                         g_free(webConfigString);
+                         AnscFreeMemory(webConfigString);
                          webConfigString = NULL;
                     }
                     free(data);
                     return RBUS_ERROR_INVALID_INPUT;
                 }
 
-                if(T2ERROR_SUCCESS != dmMsgPckProcessingCallBack((char *)webConfigString, decodedDataLen))
+                if(T2ERROR_SUCCESS != dmMsgPckProcessingCallBack(webConfigString, stringSize))
                 {
                     free(data);
                     T2Info("RBUS_ERROR_INVALID_INPUT freeing the webconfigString\n");
                     if(webConfigString != NULL) {
-                        g_free(webConfigString);
+                        AnscFreeMemory(webConfigString);
                         webConfigString = NULL;
                     }
                     return RBUS_ERROR_INVALID_INPUT;
@@ -559,7 +559,7 @@ rbusError_t t2PropertyDataGetHandler(rbusHandle_t handle, rbusProperty_t propert
             char* text;
             size = (*dmSavedMsgPackProcessingCallBack)(&temp);
             if (temp != NULL && size > 0){
-                text = g_base64_encode (temp, size);
+                text = (char *) AnscBase64Encode (temp, size);
                 T2Info("Profiles from persistant folder profile.msgpack \n");
                 rbusValue_SetString(value, text);
                 free(text);

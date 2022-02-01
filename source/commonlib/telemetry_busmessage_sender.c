a1a71ffacce19b27f655de2452879a80250d721b
Hamza Ben Zekri <hbenzekri@libertyglobal.com>
2022-02-01 16:10:31 +0100

    fix Plume not enabled when configured via bootfile ( OFW-2376 F3896SI-713 )
    [Issue]
    When plume is enabled in the bootfile, opensync does not start after
    a factory reset. This is caused by a deadlock during bootup:
    
    A telemetry API called in the WiFi-agent during bootup is doing a get
    of the Telemetry.Enable parameter, and PAM is during that time trying
    to set the SON URL but it's stuck because it's trying to run
    plume_init.sh. plume_init.sh is blocked waiting for the mesh-agent
    which still hasn't run, because it's still waiting for the wifi-agent
    to initialize
    
    [Solution]
    Make the telemetry APIs cache events if the wifi-agent hasn't been
    initialized, instead of performing a get of Telemetry.enable. Before
    this change, they only waited for PAM to be initialized.

--- source/commonlib/telemetry_busmessage_sender.c
+++ source/commonlib/telemetry_busmessage_sender.c
@@ -114,6 +114,7 @@ static void initMutex() {
         pthread_mutex_init(&sMutex, &mutexAttr);
         pthread_mutex_init(&fMutex, &mutexAttr);
         pthread_mutex_init(&dMutex, &mutexAttr);
+        pthread_mutex_init(&eventMutex, &mutexAttr);
         pthread_mutex_init(&FileCacheMutex, &mutexAttr);
         pthread_mutex_init(&markerListMutex, &mutexAttr);
         pthread_mutex_init(&loggerMutex, &mutexAttr);
@@ -342,8 +343,8 @@ static bool initRFC( ) {
         }
     }
     if((isRFCT2Enable == false) && bus_handle) {
-        //Check for PamdM status  before getting RFC_T2_ENABLE_PARAM value
-        if ((access(PAM_FILE,  F_OK) != 0)) //PAM_FILE does not exist
+        //Check for PamdM and wifi-agent status  before getting RFC_T2_ENABLE_PARAM value
+        if ((access(PAM_FILE,  F_OK) != 0) || (access( "/tmp/wifi_initialized", F_OK ) != 0)) //PAM_FILE does not exist
         {          
           EVENT_DEBUG("file pam_initialized doesn't exist..return !!! \n");
           return false;

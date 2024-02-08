8dad93bb78befee21f424c55b0a24c2a5e57f942
gps496 <gomathishankar_ps@comcast.com>
2024-02-08 07:04:12 +0000

    RDK-47288: Add WAI 2.0 support in Telemetry
    Reason for change: Update T2 with WAI RFC for Bootstrap XConfURL, PartnerName
    and osClass
    Test Procedure: Build and Verify, Ensure No Regression in WAI and T2
    Risks: Medium
    Priority: P1
    
    Change-Id: I73a9fcdf53c84b2fd6d9cb92f2bf7c6ad195d26c
    Signed-off-by: gps496 <gomathishankar_ps@comcast.com>
    (cherry picked from commit fc60f772f374759e0fd8a48a704aa0d1fc5a6855)

--- source/xconf-client/xconfclient.h
+++ source/xconf-client/xconfclient.h
@@ -32,7 +32,14 @@
 #define DEVICE_PROPERTIES                           "/etc/device.properties"
 
 #define TR181_DEVICE_MODEL                          "Device.DeviceInfo.ModelName"
+
+#if defined(WHOAMI_ENABLED)
+#define TR181_DEVICE_PARTNER_ID                     "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Bootstrap.PartnerName"
+#define TR181_DEVICE_OSCLASS                        "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Bootstrap.OsClass"
+#else
 #define TR181_DEVICE_PARTNER_ID                     "Device.DeviceInfo.X_RDKCENTRAL-COM_Syndication.PartnerId"
+#endif
+
 #define TR181_DEVICE_ACCOUNT_ID                     "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.AccountInfo.AccountID"
 #define TR181_CONFIG_URL                            "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.ConfigURL"
 
